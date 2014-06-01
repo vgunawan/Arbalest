26301fee396cf6a53e68455b942b5efd70819985
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-01 16:09:50 +1000

    fix load path

--- lib/arbalest.rb
+++ lib/arbalest.rb
@@ -1,3 +1,5 @@
+require 'arbalest/candlestick'
+
 module Arbalest
  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require_relative '../config/boot'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
