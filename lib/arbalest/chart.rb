13cd4df33a51fb5ce97607314b5496ecff270074
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-05 23:06:53 +1000

    refactor chart

--- lib/arbalest/chart.rb
+++ lib/arbalest/chart.rb
@@ -1,28 +1,20 @@
 module Arbalest
   class Chart
+    attr_reader :data
 
-    attr_reader :candles
-
-    def initialize(candles)
-      @candles = candles
+    def initialize(list)
+      @data  = []
+      list.each do |e| 
+        params = e.select { |k,v| [:o, :h, :l, :c, :v].include? k }
+        @data << { timestamp: e[:timestamp], candle: Candlestick.new(params) }
+      end
     end
 
     def at(time)
-      i = candles.find_index { |c| c[:timestamp] > time.to_i }
+      i = @data.find_index { |c| c[:timestamp] > time.to_i }
       return nil if i < 1
-      candles[i-1][:candle]
+      @data[i-1][:candle]
     end
 
-    class << self
-      def from_list(list)
-        candles = []
-        list.each do |e| 
-          params = e.select { |k,v| [:o, :h, :l, :c, :v].include? k }
-          candles << { timestamp: e[:timestamp], candle: Candlestick.new(params) }
-        end
-
-        self.new(candles)
-      end
-    end
   end
 end
--- spec/lib/chart_spec.rb
+++ spec/lib/chart_spec.rb
@@ -3,77 +3,68 @@ require 'time'
 
 module Arbalest
   describe Chart do
-    let(:candles) { [] }
 
-    subject { Chart.new(candles) } 
-    
-    it("has #candles") { expect(subject.candles).to eq(candles) }
-    
-    describe "#from_list" do
-      
-      let(:first_of_jan) { Time.parse('2014-01-01T00:00:00') }
-      subject { Chart.from_list(list) }
-      
-      context "with no volumes" do
-        let(:list) do
-          list = []
-          for i in 0..5 do
-            list << { 
-              timestamp: (first_of_jan + 3600 * i).to_i,
-              o: 170 + i, 
-              h: 190 + i, 
-              l: 160 + i, 
-              c: 175 + i }
-          end
-          list
-        end
+    let(:first_of_jan) { Time.parse('2014-01-01T00:00:00') }
+    subject { Chart.new(list) }
 
-        it("first element has timestamp") do 
-          expect(subject.candles.first[:timestamp]).to eq(list.first[:timestamp])
+    context "with no volumes" do
+      let(:list) do
+        list = []
+        for i in 0..5 do
+          list << { 
+            timestamp: (first_of_jan + 3600 * i).to_i,
+            o: 170 + i, 
+            h: 190 + i, 
+            l: 160 + i, 
+            c: 175 + i }
         end
-        
-        it("first element has a candle") do 
-          expect(subject.candles.first[:candle]).
-            to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175))
-        end
+        list
       end
+
+      it("first element has timestamp") do 
+        expect(subject.data.first[:timestamp]).to eq(list.first[:timestamp])
+      end
+
+      it("first element has a candle") do 
+        expect(subject.data.first[:candle]).
+          to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175))
+      end
+    end
+
+    let(:list) do
+      list = []
+      for i in 0..5 do
+        list << {
+          timestamp: (first_of_jan + 3600 * i).to_i, 
+          o: 170 + i, 
+          h: 190 + i, 
+          l: 160 + i,
+          c: 175 + i, 
+          v: 300 + i }
+      end
+      list
+    end
+
+    it("first element has timestamp") do 
+      expect(subject.data.first[:timestamp]).to eq(list.first[:timestamp])
+    end
+
+    it("first element has a candle") do 
+      expect(subject.data.first[:candle]).
+        to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175, v: 300))
+    end
+
+    describe "#at" do
       
-      context "with volumes" do
-        let(:list) do
-          list = []
-          for i in 0..5 do
-            list << {
-              timestamp: (first_of_jan + 3600 * i).to_i, 
-              o: 170 + i, 
-              h: 190 + i, 
-              l: 160 + i,
-              c: 175 + i, 
-              v: 300 + i }
-          end
-          list
-        end
+      it "returns closest previous candle" do
+        expect(subject.at(first_of_jan + 3599)).
+          to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175, v: 300))
+      end
 
-        it("first element has timestamp") do 
-          expect(subject.candles.first[:timestamp]).to eq(list.first[:timestamp])
-        end
-        
-        it("first element has a candle") do 
-          expect(subject.candles.first[:candle]).
-            to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175, v: 300))
-        end
-
-        describe "#at" do
-          it "returns closest previous candle" do
-            expect(subject.at(first_of_jan + 3599)).
-              to eq(Candlestick.new(o: 170, h: 190, l: 160, c: 175, v: 300))
-          end
-
-          context "no earlier data" do
-            it "returns nil" do
-              expect(subject.at(first_of_jan - 1)).
-                to be_nil
-            end
-          end
+      context "no earlier data" do
+        it "returns nil" do
+          expect(subject.at(first_of_jan - 1)).
+            to be_nil
         end
       end
 
