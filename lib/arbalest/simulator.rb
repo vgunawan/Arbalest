03a837133f59541b978d2c02e13ba3cb3a20e632
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-11 22:10:03 +1000

    implement simulator#load

--- lib/arbalest/simulator.rb
+++ lib/arbalest/simulator.rb
@@ -2,18 +2,19 @@ require 'csv'
 
 module Arbalest
   class Simulator
-    attr_reader :data, :pilot, :account
+    attr_reader :pilot, :account, :chart
 
     def initialize(pilot, account)
       @pilot = pilot
       @account = account
     end
 
-    def load(file_path)
+    def load(file_path, chart_name)
       data = []
       CSV.foreach(file_path) do |row|
-        data << MT4.parse(row)
+        data << Parsers::MT4.parse(row)
       end
+      @chart = Chart.new(data, chart_name)
     end
 
     def play
--- spec/lib/simulator_spec.rb
+++ spec/lib/simulator_spec.rb
@@ -9,13 +9,23 @@ module Arbalest
 
     describe "#load" do
       let(:path) { 'somepath' }
+      let(:data) { double('chartdata') }
+      let(:csv) { double('csv') }
+      let(:chartname) { 'gbpjpy15m' }
+
       before do
-        CSV.stub(:foreach).with(path)
-        subject.load(path)
+        CSV.stub(:foreach).with(path).and_yield(csv)
+        Parsers::MT4.stub(:parse).and_return(data)
+        Chart.stub(:new).with([data], chartname)
+        subject.load(path, chartname)
       end
 
-      it 'loads the data' do
+      it 'calls csv to read the data' do
         expect(CSV).to have_received(:foreach).with(path)
+      end
+
+      it 'sets a new chart based on the csv' do
+        expect(Chart).to have_received(:new).with([data], chartname)
       end
     end
   end
