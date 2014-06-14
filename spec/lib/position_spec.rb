160faa6893e6fccf8113e08ad4cc5f66df9214bc
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-14 22:59:50 +1000

    fix broken position spec due to refactor

--- spec/lib/position_spec.rb
+++ spec/lib/position_spec.rb
@@ -4,14 +4,14 @@ module Arbalest
   describe Position do
     let(:pair) { :eurchf }
     let(:direction) { :long }
-    let(:price) { 1.23425 }
+    let(:opening_price) { 1.23425 }
     let(:time) { Time.parse("2014-01-01T00:01:45") }
 
-    subject { Position.new(pair, direction, price, time) }
+    subject { Position.new(pair, direction, opening_price, time) }
     
     it("pair") { expect(subject.pair).to eq(pair) }
     it("direction") { expect(subject.direction).to eq(direction) }
-    it("price") { expect(subject.price).to eq(price) }
+    it("opening_price") { expect(subject.opening_price).to eq(opening_price) }
     it("time") { expect(subject.time).to eq(time) }
     it("status") { expect(subject.status).to eq(:open) }
 
