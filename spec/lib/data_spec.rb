10439b3a5cb19d049452eee06c4bbf107f04bb0e
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-16 23:08:33 +1000

    more specs for data#hit

--- spec/lib/data_spec.rb
+++ spec/lib/data_spec.rb
@@ -13,7 +13,7 @@ module Arbalest
     it('candlestick') { expect(subject.candlestick).to eq(candlestick) }
 
     describe '#hit' do
-      context 'with price out of the range' do
+      context 'with price out of the range high' do
         let(:price) { 200 }
 
         it 'returns false' do
@@ -21,8 +21,24 @@ module Arbalest
         end
       end
 
-      context 'with price in the range' do
+      context 'with price out of the range low' do
+        let(:price) { 20 }
+
+        it 'returns false' do
+          expect(subject.hit(price)).to be_false
+        end
+      end
+
+      context 'with price within the range high' do
         let(:price) { 103 }
+
+        it 'returns true' do
+          expect(subject.hit(price)).to be_true
+        end
+      end
+
+      context 'with price within the range low' do
+        let(:price) { 98 }
 
         it 'returns true' do
           expect(subject.hit(price)).to be_true
