061674ffb1f6c0280d3aa00773c1e2c8e641926f
Valen Gunawan <bakpaobulet@gmail.com>
2014-06-18 23:01:03 +1000

    implementing position#close_price,open_price,update_trail_stop

--- lib/arbalest/position.rb
+++ lib/arbalest/position.rb
@@ -17,28 +17,43 @@ module Arbalest
       @closing_price = closing_price
     end
 
+    def stop_price
+      return nil unless order.stop
+      op = (direction == :long) ? :- : :+
+      @stop_price ||= opening_price.send(op, pair.one_pip * order.stop)
+    end
+
+    def limit_price
+      return nil unless order.limit
+      op = (direction == :long) ? :+ : :-
+      @limit_price ||= opening_price.send(op, pair.one_pip * order.limit)
+    end
+
     def close_if_hit!(chart)
-      last_data = chart.last[0]
-      if last_data.hit?(limit)
-        close(:limit_hit, limit)
-      elsif last_data.hit?(stop)
-        close(:stop_hit, stop)
+      last_data = chart.last.first
+      if last_data.hit?(limit_price)
+        close(:limit_hit, limit_price)
+      elsif last_data.hit?(stop_price)
+        close(:stop_hit, stop_price)
       elsif chart.elapsed?(time_limit)
         close(:time_limit, last_data.close)
       end
     end
 
+    def update_trail_stop(chart)
+      return unless order.trail
+      last_candle = chart.last.first.candlestick
+      one_pip = order.one_pip
+      trail = order.trail
+
+      if direction == :long
+        delta = (last_candle.high - opening_price) / one_pip
+        @stop_price = stop_price + (delta.floor / trail).to_i * trail * one_pip
+      else
+      end
+    end
+
     private
-    def limit
-      op = (direction == :long) ? :+ : :-
-      opening_price.send(op, pair.one_pip * order.limit)
-    end
-
-    def stop
-      op = (direction == :long) ? :- : :+
-      opening_price.send(op, pair.one_pip * order.stop)
-    end
-
     def time_limit
       time + order.time_limit
     end
--- spec/lib/position_spec.rb
+++ spec/lib/position_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 module Arbalest
   describe Position do
-    let(:pair) { double('eurchf') }
+    let(:pair) { double('eurchf', one_pip: one_pip) }
+    let(:one_pip) { 0.0001 }
     let(:direction) { :long }
     let(:opening_price) { 1.23425 }
     let(:time) { Time.parse("2014-01-01T00:01:45") }
-    let(:order) { double('order') }
+    let(:order) { double('order', stop: nil, limit: nil) }
 
     subject { Position.new(pair, direction, opening_price, time, order) }
-    
+
     it("pair") { expect(subject.pair).to eq(pair) }
     it("direction") { expect(subject.direction).to eq(direction) }
     it("opening_price") { expect(subject.opening_price).to eq(opening_price) }
@@ -47,7 +48,6 @@ module Arbalest
         before do
           allow(data).to receive(:hit?).with(expected_limit).and_return(true)
           allow(order).to receive(:limit).and_return(limit)
-          allow(pair).to receive(:one_pip).and_return(one_pip)
           subject.stub(:close)
           subject.close_if_hit!(chart)
         end
@@ -68,8 +68,7 @@ module Arbalest
           allow(data).to receive(:hit?).with(limit).and_return(false)
           allow(data).to receive(:hit?).with(expected_stop).and_return(true)
           allow(order).to receive(:stop).and_return(stop)
-          allow(pair).to receive(:one_pip).and_return(one_pip)
-          subject.stub(:limit).and_return(limit)
+          subject.stub(:limit_price).and_return(limit)
           subject.stub(:close)
           subject.close_if_hit!(chart)
         end
@@ -94,14 +93,147 @@ module Arbalest
             with(position_time_limit).and_return(true)
           allow(data).to receive(:close).and_return(closing_price)
           allow(order).to receive(:time_limit).and_return(time_limit)
-          subject.stub(:limit).and_return(limit)
-          subject.stub(:stop).and_return(stop)
+          subject.stub(:limit_price).and_return(limit)
+          subject.stub(:stop_price).and_return(stop)
           subject.stub(:close)
           subject.close_if_hit!(chart)
         end
 
         it 'close the position' do
           expect(subject).to have_received(:close).with(:time_limit, closing_price)
+        end
+      end
+    end
+    
+    describe 'stop level' do
+      context 'with no stop set' do
+        before do
+          allow(order).to receive(:stop).and_return(nil)
+        end
+
+        it 'returns nil' do
+          expect(subject.stop_price).to be_nil
+        end
+      end
+
+      context 'with a long position' do
+        let(:stop) { 10 }
+        let(:stop_price) { opening_price - one_pip * stop }
+        let(:direction) { :long }
+
+        before do
+          allow(subject).to receive(:direction).and_return(direction)
+          allow(order).to receive(:stop).and_return(stop)
+        end
+
+        it 'returns the stop level' do
+          expect(subject.stop_price).to eq(stop_price)
+        end
+      end
+
+      context 'with short position' do
+        let(:stop) { 10 }
+        let(:stop_price) { opening_price + one_pip * stop }
+        let(:direction) { :short }
+
+        before do
+          allow(subject).to receive(:direction).and_return(direction)
+          allow(order).to receive(:stop).and_return(stop)
+        end
+
+        it 'returns the stop level' do
+          expect(subject.stop_price).to eq(stop_price)
+        end
+      end
+    end
+
+    describe 'limit level' do
+      context 'with no limit set' do
+        before do
+          allow(order).to receive(:limit).and_return(nil)
+        end
+
+        it 'returns nil' do
+          expect(subject.limit_price).to be_nil
+        end
+      end
+
+      context 'with long position' do
+        let(:limit) { 10 }
+        let(:limit_price) { opening_price + one_pip * limit }
+        let(:direction) { :long }
+        
+        before do
+          allow(subject).to receive(:direction).and_return(direction)
+          allow(order).to receive(:limit).and_return(limit)
+        end
+
+        it 'returns the stop level' do
+          expect(subject.limit_price).to eq(limit_price)
+        end
+      end
+
+      context 'with short position' do
+        let(:limit) { 10 }
+        let(:limit_price) { opening_price - one_pip * limit }
+        let(:direction) { :short }
+        
+        before do
+          allow(subject).to receive(:direction).and_return(direction)
+          allow(order).to receive(:limit).and_return(limit)
+        end
+        it 'returns the stop level' do
+          expect(subject.limit_price).to eq(limit_price)
+        end
+      end
+    end
+
+    describe '#update_trail_stop' do
+      let(:data) { double('data') } 
+      let(:chart) { double('chart', last: [data]) }
+      let(:opening_price) { 1.00 }
+      let(:stop_price) { opening_price - one_pip * stop }
+      let(:direction) { :long }
+
+      before do
+        allow(subject).to receive(:direction).and_return(direction)
+      end
+
+      context 'with no trail specified' do
+        let(:stop) { 10 }
+        let(:order) { double('order', stop: 10, trail: nil) }
+
+        it 'does not update stop' do
+          subject.update_trail_stop(chart)
+          expect(subject.stop_price).to eq(stop_price)
+        end
+      end
+
+      context 'price moved more than the trail' do
+        let(:trail) { 5 }
+        let(:expected_price) { opening_price }
+        let(:order) { double('order', one_pip: one_pip, trail: trail, stop: 5) }
+        let(:high) { 1.00081 }
+        let(:candle) { double('candle', high: high) }
+        let(:data) { double('data', candlestick: candle) }
+
+        it 'adjust the stop' do
+          subject.update_trail_stop(chart)
+          expect(subject.stop_price).to eq(expected_price)
+        end
+      end
+
+      context 'price moved more than twice the trail' do
+        let(:trail) { 5 }
+        let(:expected_price) { opening_price + trail * one_pip }
+        let(:order) { double('order', one_pip: one_pip, trail: trail, stop: 5) }
+        let(:high) { 1.00101 }
+        let(:candle) { double('candle', high: high) }
+        let(:data) { double('data', candlestick: candle) }
+
+        it 'adjust the stop' do
+          subject.update_trail_stop(chart)
+          expect(subject.stop_price).to eq(expected_price)
         end
       end
     end
